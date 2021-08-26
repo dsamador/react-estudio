878d32fe5612e9831001da9c2db1a261e640b918
David Amador <54488581+dsamador@users.noreply.github.com>
2021-08-25 20:59:05 -0500

    Se configura la navegación

--- mern-lista/client/src/components/Navigation.js
+++ mern-lista/client/src/components/Navigation.js
@@ -1,9 +1,28 @@
-import { Navbar, Nav, NavDropDown } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import {NavLink} from 'react-router-dom'
 
 export default function Navigation() {
     return (
         <Navbar collapseOnSelect expand="lg" variant="dark" bg="dark">
-            <h1>Nav</h1>
+            <Navbar.Brand as={NavLink} to="/">
+                Task Manager
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link as={NavLink} to="/projects">
+                        Proyectos
+                    </Nav.Link>
+                    <NavDropdown title="Admin">
+                        <NavDropdown.Item as={NavLink} to="/admin/users">Usuarios</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Nav>
+                    <Nav.Link as={NavLink} to="/login">Iniciar Sesión</Nav.Link>
+                    <Nav.Link as={NavLink} to="/register">Registrarse</Nav.Link>
+                    <Nav.Link as={NavLink} to="/account">Mi cuenta</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Navbar>
     )
 }
